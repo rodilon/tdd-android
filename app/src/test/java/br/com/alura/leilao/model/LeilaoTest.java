bb2908d73e27a2d29a109e5e992d3ba0cf483257
rodilon <rodilon@brq.com>
2019-03-27 12:45:38 -0300

    - criando testes para diferentes ordens de entrada de dados para o valor de um leilao. descrevendo com clareza o que cada teste realiza.

--- app/src/test/java/br/com/alura/leilao/model/LeilaoTest.java
+++ app/src/test/java/br/com/alura/leilao/model/LeilaoTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.*;
 public class LeilaoTest {
 
     @Test
-    public void getDescricao() {
+    public void deve_DevolverDescricao_QuandoRecebeDescricao() {
         //criar cenário
         Leilao console = new Leilao("Console");
 
@@ -19,12 +19,34 @@ public class LeilaoTest {
     }
 
     @Test
-    public void getMaiorLance(){
+    public void deve_DevolverMaiorLance_QuandoApenasUmLance() {
         Leilao console = new Leilao("Console");
         console.propoe(new Lance(new Usuario("Alex"), 200.0));
 
         double maiorLanceDevolvido = console.getMaiorLance();
 
         assertEquals(200.0, maiorLanceDevolvido, 0.0001);
+    }
+
+    @Test
+    public void deve_DevolverMaiorLance_QuandoRecebeMaisDeUmLance_EmOrdemCrescente() {
+        Leilao computador = new Leilao("Computador");
+        computador.propoe(new Lance(new Usuario("Alex"), 200.0));
+        computador.propoe(new Lance(new Usuario("Fran"), 300.0));
+
+        double maiorLanceDevolvidoDoComputador = computador.getMaiorLance();
+
+        assertEquals(300.0, maiorLanceDevolvidoDoComputador, 0.0001);
+    }
+
+    @Test
+    public void deve_DevolverMaiorLance_QuandoRecebeMaisDeUmLance_EmOrdemDecrescente() {
+        Leilao casa = new Leilao("Casa");
+        casa.propoe(new Lance(new Usuario("Alex"), 11000.0));
+        casa.propoe(new Lance(new Usuario("Fran"), 9000.0));
+
+        double maiorLanceDevolvidoDaCasa = casa.getMaiorLance();
+
+        assertEquals(11000.0, maiorLanceDevolvidoDaCasa, 0.0001);
     }
 }
